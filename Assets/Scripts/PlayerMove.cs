0d1afde94a2ec6b04267576f4121f9ee86b9b6bb
mehran iran <meh.aghagany1378037@gmail.com>
2022-04-27 21:35:15 +0430

    +move forward and directionl

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    [SerializeField] float forward_speed=5;
+    [SerializeField] float direction_speed=10;
+    float line_x = 3.5f;
+    int line_number = 0;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // this.transform.Translate(Vector3.forward* speed * Time.deltaTime,Space.World);
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            line_number++;
+            if(line_number > 1)
+            {
+                line_number = 1;
+            }
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+           line_number--;
+            if(line_number < -1)
+            {
+                line_number = -1;
+            }
+        }
+
+        this.transform.Translate(Vector3.forward * forward_speed * Time.deltaTime, Space.World);
+        transform.position = Vector3.MoveTowards(transform.position,  new Vector3(line_x*line_number, transform.position.y, transform.position.z), Time.deltaTime * direction_speed);
+
+
+    }
+   
+}
