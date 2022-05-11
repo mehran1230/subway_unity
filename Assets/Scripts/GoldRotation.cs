eeadd1c684df47744f2ac812e584a4cb6df4740a
mehran iran <meh.aghagany1378037@gmail.com>
2022-05-11 22:02:04 +0430

    +add sounds +lose +gold system

---
+++ Assets/Scripts/GoldRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldRotation : MonoBehaviour
+{
+    [SerializeField] float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(90,0,0)*speed*Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,36 +7,66 @@ public class PlayerMove : MonoBehaviour
 
     [SerializeField] float forward_speed=5;
     [SerializeField] float direction_speed=10;
+    [SerializeField] AudioSource coin;
+    [SerializeField] AudioSource hit;
+    [SerializeField] AudioSource back_ground_music;
+    [SerializeField] int number_of_gold = 0;
+    Animator player_animator;
     float line_x = 3.5f;
     int line_number = 0;
+    bool is_alive = true;
     void Start()
     {
+        player_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // this.transform.Translate(Vector3.forward* speed * Time.deltaTime,Space.World);
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+        if (is_alive)
         {
-            line_number++;
-            if(line_number > 1)
+            // this.transform.Translate(Vector3.forward* speed * Time.deltaTime,Space.World);
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                line_number = 1;
+                line_number++;
+                if (line_number > 1)
+                {
+                    line_number = 1;
+                }
             }
-        }else if(Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-           line_number--;
-            if(line_number < -1)
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                line_number = -1;
+                line_number--;
+                if (line_number < -1)
+                {
+                    line_number = -1;
+                }
             }
+
+            this.transform.Translate(Vector3.forward * forward_speed * Time.deltaTime, Space.World);
+            transform.position = Vector3.MoveTowards(transform.position, new Vector3(line_x * line_number, transform.position.y, transform.position.z), Time.deltaTime * direction_speed);
         }
 
-        this.transform.Translate(Vector3.forward * forward_speed * Time.deltaTime, Space.World);
-        transform.position = Vector3.MoveTowards(transform.position,  new Vector3(line_x*line_number, transform.position.y, transform.position.z), Time.deltaTime * direction_speed);
-
-
     }
-   
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("gold"))
+        {
+            coin.Play();
+            Destroy(other.gameObject);
+            number_of_gold++;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("obstacle")) {
+            Debug.Log("Lose");
+            is_alive = false;
+            back_ground_music.Stop();
+
+            hit.Play();
+            player_animator.Play("lose");
+        }
+    }
+
 }
